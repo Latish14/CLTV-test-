0be9bc3d4b0b9e08e29d3503a15014c43394b912
Latish14 <160815195+Latish14@users.noreply.github.com>
2025-04-11 18:05:48 +0530

    Update Untitled1.py

--- Untitled1.py
+++ Untitled1.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPJ5uLUekS3EwOmc9UGavLX",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
